bf9ed77d147f53663e9763b2c1e2c938997eedaf
seymur4ik <146814373+seymur4ik@users.noreply.github.com>
2024-05-19 23:54:38 +0400

    Last upd + chg

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import ClassRoom from "./0-classroom.js";
+export default function initializeRooms() {
+    const clsRoom = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+    return clsRoom;
+}
---
+++ ES6_classes/README.md
@@ -0,0 +1,1 @@
+README FOR ES6 classes
